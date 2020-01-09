9a0ca7e3f586df554f01bbdad7cb432272e00aa4
AlanLi1091 <alanli931108@hotmail.com>
2020-01-09 15:33:19 -0800

    Added test objects

---
+++ project.py
@@ -0,0 +1,2 @@
+destinations = ["Paris, France", "Shanghai, China PR", "Los Angeles, USA", "Sao Paulo, Brazil", "Cairo, Egypt"]
+test_traveler = ['Erin Wilkes', 'Shanghai, China PR', ['historical site', 'art']]
